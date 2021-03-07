aface8af5f492870141677f20a19b32ac719a52f
Anima7ron <george@forzaesotico.com>
2021-03-06 22:30:06 -0600

    Complete

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,10 @@ fn main() {
     println!("A nickel is worth {} cents.", value_in_cents(n));
     println!("A dime is worth {} cents.", value_in_cents(d));
     println!("A quarter is worth {} cents.", value_in_cents(q));
+
+    let five = Some(5);
+    let _six = plus_one(five);
+    let _none = plus_one(None);
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -23,5 +27,18 @@ fn value_in_cents(coin: Coin) -> u8 {
         Coin::Nickel => 5,
         Coin::Dime => 10,
         Coin::Quarter => 25,
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => {
+           println!("Found None, Returned None");
+           None
+        },
+        Some(i) => {
+            println!("Found {0}, returned {0} + 1", i);
+            Some(i + 1)
+        },
     }
 }
